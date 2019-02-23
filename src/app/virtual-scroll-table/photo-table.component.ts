7eb197fb0311dcf7b97563b95b36691d9ea0b1dc
Connie Leung <cleung334@gmail.com>
2019-02-23 23:24:42 +0800

    feat:  make basic material table

--- src/app/virtual-scroll-table/photo-table.component.ts
+++ src/app/virtual-scroll-table/photo-table.component.ts
@@ -1,22 +1,36 @@
+import { VIRTUAL_SCROLL_STRATEGY } from '@angular/cdk/scrolling';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { IPhoto, PhotosService } from '../services/photos-service';
+import { TableVirtualScrollStrategy } from './table-virtual-scroll-strategy';
 
 @Component({
   selector: 'app-photo-table',
   templateUrl: './photo-table.component.html',
   styleUrls: ['./photo-table.component.scss'],
+  providers: [
+    {
+      provide: VIRTUAL_SCROLL_STRATEGY,
+      useClass: TableVirtualScrollStrategy,
+    },
+  ],
 })
 export class PhotoTableComponent implements OnInit, OnDestroy {
   photos$: Observable<IPhoto[]>;
-  destroy$ = new Subject();
+  destroy$ = new Subject<void>();
   dataSource: Observable<IPhoto[]>;
+
+  displayedColumns: string[] = ['id', 'albumId', 'title', 'url', 'thumbnailUrl'];
 
   constructor(private service: PhotosService) {}
 
   ngOnInit() {
-    this.photos$ = this.service.getAll$().pipe(takeUntil(this.destroy$));
+    this.photos$ = this.service.getBatch$(1);
+    this.dataSource = combineLatest(this.photos$).pipe(
+      map(values => values[0]),
+      takeUntil(this.destroy$),
+    );
   }
 
   ngOnDestroy() {
--- src/app/virtual-scroll-table/table.module.ts
+++ src/app/virtual-scroll-table/table.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material';
 import { SharedModule } from '../shared';
 import { PhotoTableComponent } from './photo-table.component';
 import { TableRoutingModule } from './table-routing.module';
 
 @NgModule({
   declarations: [PhotoTableComponent],
-  imports: [CommonModule, TableRoutingModule, SharedModule],
+  imports: [CommonModule, TableRoutingModule, SharedModule, MatTableModule],
   exports: [PhotoTableComponent],
 })
 export class TableModule {}
